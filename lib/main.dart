493e1cc8c9b73655774b7bac191921a2c190d55a
Eugene <sokol.joe@gmail.com>
2021-05-18 23:35:28 +0300

    bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expenses/widgets/user_transactions.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
+import 'models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,12 +19,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'New thing', amount: 15.0, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New second thing', amount: 5.0, date: DateTime.now()),
+  ];
+
+  void _addUserTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addUserTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
         title: Text('Flutter app'),
       ),
       body: SingleChildScrollView(
@@ -38,13 +78,13 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5.0,
               ),
             ),
-            UserTransactions(),
+            TransactionList(transactions: _userTransactions),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
