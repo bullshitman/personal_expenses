5497acb96321c82456e70e2d7fe2cfd2329bf32b
Eugene <sokol.joe@gmail.com>
2021-06-04 23:19:42 +0300

    sicked

--- lib/widgets/new_widget.dart
+++ lib/widgets/new_widget.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class NewWidget extends StatelessWidget {
+class NewWidget extends StatefulWidget {
   const NewWidget({Key key}) : super(key: key);
 
+  @override
+  _NewWidgetState createState() => _NewWidgetState();
+}
+
+class _NewWidgetState extends State<NewWidget> {
   @override
   Widget build(BuildContext context) {
     return Container();
